6b1f00810e012bbf2a08c97ee68d46dab4608691
oyy0v0 <oyy0v0@163.com>
2025-02-11 16:00:06 +0800

    实现了管理员批量抓取图片页面

--- src/api/typings.d.ts
+++ src/api/typings.d.ts
@@ -5,6 +5,12 @@ declare namespace API {
     msg?: string
   }
 
+  type BaseResponseInt_ = {
+    code?: number
+    data?: number
+    msg?: string
+  }
+
   type BaseResponseLoginUserVO_ = {
     code?: number
     data?: LoginUserVO
@@ -204,9 +210,16 @@ declare namespace API {
     tags?: string[]
   }
 
+  type PictureUploadByBatchRequest = {
+    count?: number
+    namePrefix?: string
+    searchText?: string
+  }
+
   type PictureUploadRequest = {
     fileUrl?: string
     id?: number
+    picName?: string
   }
 
   type PictureVO = {
@@ -236,6 +249,7 @@ declare namespace API {
   type uploadPictureUsingPOSTParams = {
     fileUrl?: string
     id?: number
+    picName?: string
   }
 
   type User = {
